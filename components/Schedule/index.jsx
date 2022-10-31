e6fcf9a82126f0792b2418639dc38629dda2bee2
Gergely Gazda <info@gazdagergo.com>
2022-10-31 07:02:07 +0100

    book to calendar

--- components/Schedule/index.jsx
+++ components/Schedule/index.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react'
+import ICalendarLink from "react-icalendar-link";
 import useFlow from '../../hooks/useFlow'
 import useSpeech from "../../hooks/useSpeech"
 import useSpeechRecognition from '../../hooks/useSpeechRecognition'
 
 const slots = [
-  { timeString: "2012-11-02T09:00:00+02:00", title: "E hét szerda 9 óra" },
-  { timeString: "2012-11-04T15:00:00+02:00", title: "E hét péntek 3 óra" },
-  { timeString: "2012-11-08T12:00:00+02:00", title: "Jövő kedd 12 óra" },
+  { timeString: "2022-11-02T09:00:00+01:00", title: "E hét szerda 9 óra" },
+  { timeString: "2022-11-04T15:00:00+01:00", title: "E hét péntek 3 óra" },
+  { timeString: "2022-11-08T12:00:00+01:00", title: "Jövő kedd 12 óra" },
 ]
 
 const Schedule = () => {
@@ -37,11 +38,15 @@ const Schedule = () => {
     }
   })
 
+  const [isCalling, setIsCalling] = useState(false)
   const [selectedSlot, setselectedSlot] = useState(null)
+  const [confirmed, setConfirmed] = useState(false)
 
 
   useEffect(() => {
-    confirmSelectedSlot()
+    if (selectedSlot){
+      confirmSelectedSlot()
+    }
   }, [selectedSlot])
 
 
@@ -49,41 +54,45 @@ const Schedule = () => {
     let offerAccepted
     for (const slot of slots) {
       await say(slot.title)
-      await say('Foglaláshoz mondjon igent')
       if (await waitForAnswer() === 'igen') {
         offerAccepted = true
         setselectedSlot(slot)
         break
       }
+
     }
-    if (!offerAccepted) {
-      await say('Ön nem választott időpontot.')
-    }
+   if (!offerAccepted) {
+    await say('Ön nem választott időpontot. Viszontlátásra.')
+    setIsCalling(false)
+   }
   }
 
   const confirmSelectedSlot = async () => {
     await say(`Köszönöm. Ön a következő időpontot választotta: ${selectedSlot?.title}`)
     await say('A megerősítéshez mondjon igent')
     if (await waitForAnswer() === 'igen') {
+      setConfirmed(true)
       await say(`
         Köszönöm.
         A foglalás megerősítve.
         Várjuk Önt a következő időpontban: ${selectedSlot?.title}.
         A viszontlátásra.
       `)
+      setIsCalling(false)
     } else {
-      await say('Ön nem erősítette meg a foglalást')
+      await say('Ön nem erősítette meg a foglalást. A foglalás sikertelen. Viszontlátásra.')
+      setIsCalling(false)
     }
   }
 
 
   const hancdleStartClick = async () => {
-    // await say("A szabad időpontok:")
+    setIsCalling(true)
     await say(`
       Üdvözlöm, Gedeon bácsi fodrászüzletét hívta.
       Én egy virtuális asszisztens vagyok, nálam tud időpontot foglalni, hogy ne zavarjuk a mestert munka közben.
       Felsorolom önnek a szabad időpontokat, amelyekből választhat.
-      Kérjük, a kiválasztáshoz mondjon igent az időpont elhangzása után.
+      Amennyiben megfelel az időpont, mondja azt, hogy igen.
     `)
     await sayOffers()
   }
@@ -93,14 +102,22 @@ const Schedule = () => {
       <p>Foglaljon időpontot virtuális asszisztensünknél</p>
       <button
         onClick={hancdleStartClick}
+        disabled={isCalling}
         >
         Mehet
       </button>
-      <button
-        onClick={stopSpeaking}
-        >
-        Megszakít  
-      </button>
+      <br /><br />
+      {confirmed && (
+        <ICalendarLink event={{
+          title: "Fodrász",
+          startTime: selectedSlot.timeString,
+          location: "Lövőház utca 2-6, Budapest",
+        }}>
+          <button>
+            Hozzáadás a naptáramhoz
+          </button>
+        </ICalendarLink>
+      )}
     </section>
   )
 }

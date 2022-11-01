5590609131dd61e0d81b99ea8263b03165e6d12a
Gergely Gazda <info@gazdagergo.com>
2022-11-02 00:14:21 +0100

    fix confirmation

--- components/Schedule/index.jsx
+++ components/Schedule/index.jsx
@@ -1,8 +1,22 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import ICalendarLink from "react-icalendar-link";
 import useFlow from '../../hooks/useFlow'
 import useSpeech from "../../hooks/useSpeech"
 import useSpeechRecognition from '../../hooks/useSpeechRecognition'
+
+const slots = [
+  { start: "2022-11-09T09:00:00+01:00" },
+  { start: "2022-11-11T15:00:00+01:00" },
+  { start: "2022-11-12T12:00:00+01:00" },
+]
+
+const steps = {
+  IDLE: 'Idle',
+  WELCOME: 'Welcome text',
+  OFFERING: 'Offering slots',
+  SLOT_SELECTED: 'Asking for confirmation',
+  NOT_CHOOSEN: 'No slots left and no slots has been choosen',
+}
 
 const getReadableDateTime = (timeString, locale = 'en-US') => {
   const date = new Date(timeString)
@@ -11,112 +25,131 @@ const getReadableDateTime = (timeString, locale = 'en-US') => {
   return `${day}, ${time}`
 }
 
-const slots = [
-  { timeString: "2022-11-09T09:00:00+01:00", title: getReadableDateTime("2022-11-09T09:00:00+01:00") },
-  { timeString: "2022-11-11T15:00:00+01:00", title: getReadableDateTime("2022-11-11T15:00:00+01:00") },
-  { timeString: "2022-11-12T12:00:00+01:00", title: getReadableDateTime("2022-11-12T12:00:00+01:00") },
-]
-
 const Schedule = () => {
   const { say, stopSpeaking } = useSpeech()
   const { wait } = useFlow()
-  const userAnswer = useRef('')
 
-  const handleGotAnswer = (answer) => {
-    userAnswer.current = answer
-  }
+  const [step, setStep] = useState('IDLE')
+  const [isCalling, setIsCalling] = useState(false)
+  const [userAnswer, setUserAsnwer] = useState('')
+  const [offeredSlotIndex, setOfferedSlotIndex] = useState(null)
+  const [selectedSlot, setselectedSlot] = useState(null)
+  const [confirmed, setConfirmed] = useState(false)
 
   const {
-    setisRecording
+    setisRecording,
+    isRecording
   } = useSpeechRecognition({
-    onResult: handleGotAnswer,
+    onResult: setUserAsnwer, // TODO: have to throttle this as multipl
     options: {
       continuous: false,
       interimResults: false,
     }
   })
 
-  const [isCalling, setIsCalling] = useState(false)
-  const [selectedSlot, setselectedSlot] = useState(null)
-  const [confirmed, setConfirmed] = useState(false)
-
-  const waitForAnswer = useCallback(async (time = 3) => {
+  const startListening = () => {
+    setUserAsnwer(() => '')
     setisRecording(true)
-    await wait(time)
-    setisRecording(false)
-    const answer = userAnswer.current
-    userAnswer.current = ''
-    return answer
-  }, [setisRecording, wait])  
+  }
 
-  const confirmSelectedSlot = useCallback(async () => {
-    await say(`Thank you. You have choosen the: ${selectedSlot?.title}`)
-    await say('To confirm, please, say yes')
-    if ((await waitForAnswer()).toLowerCase() === 'yes') {
-      setConfirmed(true)
-      await say(`
-        Thank you.
-        Your appointment has been confirmed.
-        See you on: ${selectedSlot?.title}.
-        Good bye.
-      `)
-      setIsCalling(false)
-    } else {
-      await say('You did not confirm.')
-      setIsCalling(false)
-    }
-  }, [say, waitForAnswer, setConfirmed, setIsCalling, selectedSlot])
+  const stopListening = () => {
+    setUserAsnwer(() => '')
+    setisRecording(false)
+  }
 
   useEffect(() => {
-    if (selectedSlot){
-      confirmSelectedSlot()
-    }
-  }, [selectedSlot, confirmSelectedSlot])
-
-
-  const sayOffers = async () => {
-    let offerAccepted
-    for (const slot of slots) {
-      await say(slot.title)
-      if ((await waitForAnswer()).toLowerCase() === 'yes') {
-        offerAccepted = true
-        setselectedSlot(slot)
-        break
+    if (offeredSlotIndex !== null && !selectedSlot) {
+      if (!slots[offeredSlotIndex]) {
+        setStep('NOT_CHOOSEN')
+        say('You have not choosen any slot. Good bye.').then(() => {
+          stopListening()
+          setIsCalling(false)
+          setStep('IDLE')
+        })
+        return
       }
 
+      say(getReadableDateTime(slots[offeredSlotIndex].start))
+      .then(() => {
+        startListening()
+        return wait(3)
+      })
+      .then(() => {
+        stopListening()
+        // TODO: it steps one loop cycle forward unnecessarily
+        setOfferedSlotIndex(prevIndex => prevIndex + 1)
+      })
     }
-   if (!offerAccepted) {
-    await say('You have not choosen any slot. Good bye.')
-    setIsCalling(false)
-   }
+  }, [offeredSlotIndex])
+
+  const saidYes = () => {
+    return ['yes', 'yeah'].includes(userAnswer.toLowerCase())
   }
 
+  useEffect(() => {
+    if (step === 'SLOT_SELECTED' && saidYes()) {
+      stopListening()
+      setConfirmed(true)
+      setStep('IDLE')
+      say(`Your appointment has been confirmed. See you on ${getReadableDateTime(selectedSlot?.start)}. Good bye.`).then(() => {
+        setIsCalling(false)
+      })
+    }
 
-  const hancdleStartClick = async () => {
-    setIsCalling(true)
-    await say(`
-      Hello. You are calling the hair dresser shop of Gideon.
-      I am a virtual assistant and I will help you to book a slot for you.
-      I will offer you the available slots.
-      To choose one, please, say yes.
-    `)
-    await sayOffers()
+    if (step === 'OFFERING' && saidYes()) {
+      stopListening()
+      setselectedSlot(slots[offeredSlotIndex])
+      setStep('SLOT_SELECTED')
+    }
+  }, [step, userAnswer])
+
+  const confirmSelectedSlot = async () => {
+    await say(`Thank you. You have choosen the: ${getReadableDateTime(selectedSlot?.start)}`)
+    await say('To confirm, please, say yes')
+    startListening()
   }
+
+  useEffect(() => {
+    if (selectedSlot && !confirmed) {
+      confirmSelectedSlot()
+    }
+  }, [selectedSlot])
+
+
+
+  useEffect(() => {
+    if (step === 'WELCOME') {
+      setIsCalling(true)
+      say(`
+        Hello. You are calling the hair dresser shop of Gideon.
+        I am a virtual assistant and I will help you to book a slot for you.
+        I will offer you the available slots.
+        To choose one, please, say yes.
+      `).then(() => setStep('OFFERING'))
+    }
+    if (step === 'OFFERING') {
+      setOfferedSlotIndex(0)
+    }
+  }, [step])
 
   return (
     <section>
       <p>Book a slot with the help of our virtual assistant</p>
       <button
-        onClick={hancdleStartClick}
+        onClick={() => setStep('WELCOME')}
         disabled={isCalling}
         >
         Start
       </button>
+      <p style={{ color: 'gray' }}>Status: {steps[step]}</p>
+      {!selectedSlot && offeredSlotIndex !== null && slots[offeredSlotIndex] && <p>{getReadableDateTime(slots[offeredSlotIndex]?.start)}</p>}
+      {selectedSlot && <p>{getReadableDateTime(selectedSlot?.start)}</p>}
+      {isRecording && <p>Listening your answer...</p>}
       <br /><br />
       {confirmed && (
         <ICalendarLink event={{
           title: "Hair dresser",
-          startTime: selectedSlot.timeString,
+          startTime: selectedSlot.start,
           location: "Budapest, Kis Rókus u. 16-20, 1024",
         }}>
           <button>
